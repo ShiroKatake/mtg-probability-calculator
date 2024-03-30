25685b90d3e33947b65bc704a4cddbd23b9aef3f
ShiroKatake <tgkhang262@gmail.com>
2024-03-31 01:48:46 +1100

    fix(hypergeometric): moved rounding and percentage conversion to output

--- app/utils/hypergeometric/hypergeometric.ts
+++ app/utils/hypergeometric/hypergeometric.ts
@@ -1,5 +1,4 @@
 import { combination } from "../combination/combination";
-import { round } from "../round/round";
 
 /**
  * Calculates the probability of x successes in n draws from a population of size N,
@@ -13,10 +12,7 @@ import { round } from "../round/round";
  */
 
 export const hypergeometric = (N: number, n: number, k: number, x: number) => {
-  const probability = combination(k, x) * combination(N - k, n - x) / combination(N, n);
-  const result = round(probability, 3);
-  console.log(result);
-  return result;
+  return combination(k, x) * combination(N - k, n - x) / combination(N, n);
 };
 
 export const dropMiss = (initialSuccessesInDeck: number, deckSize: number = 99, initialHandsize: number = 7) => {
