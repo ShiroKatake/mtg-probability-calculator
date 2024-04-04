457690122a3dc4fc7ef5144c870d0181d30e2297
ShiroKatake <tgkhang262@gmail.com>
2024-04-04 14:02:27 +1100

    fix(hypergeometric test): added rounding to truncate test result

--- app/utils/hypergeometric/hypergeometric.spec.ts
+++ app/utils/hypergeometric/hypergeometric.spec.ts
@@ -1,12 +1,13 @@
+import { round } from '../round/round';
 import { hypergeometric } from './hypergeometric';
 
 describe('Hypergeometric distribution calculator', () => {
   it('should return the expected probability', () => {
     // Resulted cross checked at https://aetherhub.com/Apps/HyperGeometric
-    expect(hypergeometric(60, 7, 4, 1)).toBe(0.336);
+    expect(round(hypergeometric(60, 7, 4, 1), 3)).toBe(0.336);
   });
   it('should return the expected probability', () => {
     // Resulted cross checked at https://aetherhub.com/Apps/HyperGeometric
-    expect(hypergeometric(99, 7, 10, 1)).toBe(0.537);
+    expect(round(hypergeometric(99, 7, 10, 1), 3)).toBe(0.390);
   });
 });
