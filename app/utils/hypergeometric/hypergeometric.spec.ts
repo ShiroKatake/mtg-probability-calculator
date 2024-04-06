9e4baa546ba5928ce7a70f393718550fa8db1c5d
ShiroKatake <tgkhang262@gmail.com>
2024-04-07 01:12:29 +1100

    fix(hypergeometric): touched up functionality

--- app/utils/hypergeometric/hypergeometric.spec.ts
+++ app/utils/hypergeometric/hypergeometric.spec.ts
@@ -1,13 +1,20 @@
 import { round } from '../round/round';
-import { hypergeometric } from './hypergeometric';
+import { dropMiss, hypergeometric, hypergeometricKOrMore } from './hypergeometric';
 
 describe('Hypergeometric distribution calculator', () => {
   it('should return the expected probability', () => {
     // Resulted cross checked at https://aetherhub.com/Apps/HyperGeometric
-    expect(round(hypergeometric(60, 7, 4, 1), 3)).toBe(0.336);
+    expect(round(hypergeometric(99, 7, 10, 1), 3)).toBe(0.390);
   });
   it('should return the expected probability', () => {
     // Resulted cross checked at https://aetherhub.com/Apps/HyperGeometric
-    expect(round(hypergeometric(99, 7, 10, 1), 3)).toBe(0.390);
+    expect(round(hypergeometricKOrMore(60, 7, 4, 1), 3)).toBe(0.399);
+  });
+  it('should return the expected probability', () => {
+    // Resulted cross checked at https://mtg.dawnglare.com/?p=lands&decksize=99&landmin=37&landmax=37&carddraw=8&maxturns=15
+    expect(round(hypergeometricKOrMore(99, 9, 37, 2), 3)).toBe(0.916);
+  });
+  it('should return the expected probability', () => {
+    expect(dropMiss(37)).toBe(5);
   });
 });
--- app/utils/hypergeometric/hypergeometric.ts
+++ app/utils/hypergeometric/hypergeometric.ts
@@ -6,21 +6,30 @@ import { combination } from "../combination/combination";
  *
  * @param N Size of the population
  * @param n Number of samples drawn from the population
- * @param k Number of successes in sample
- * @param x Number of successes in sample drawn
+ * @param K Number of successes in sample
+ * @param k Number of successes in sample drawn
  * @returns The probability of x successes in n draws from a population of size N
  */
 
-export const hypergeometric = (N: number, n: number, k: number, x: number) => {
-  return combination(k, x) * combination(N - k, n - x) / combination(N, n);
+export const hypergeometric = (N: number, n: number, K: number, k: number) => {
+  return combination(K, k) * combination(N - K, n - k) / combination(N, n);
 };
 
-export const dropMiss = (initialSuccessesInDeck: number, deckSize: number = 99, initialHandsize: number = 7) => {
+export const hypergeometricKOrMore = (N: number, n: number, K: number, k: number) => {
   let probability = 0;
+  for (let i = 0; i < k; i++) {
+    probability += hypergeometric(N, n, K, i);
+  }
+  probability = 1 - probability;
+  return probability;
+};
+
+export const dropMiss = (K: number, N: number = 99, n: number = 7) => {
+  let probability = 1;
   let turn = 0;
-  while (probability > 0.5) {
-    probability = hypergeometric(deckSize, initialHandsize + turn, initialSuccessesInDeck, turn);
+  while (probability >= 0.5) {
     turn++;
+    probability = hypergeometricKOrMore(N, n + turn, K, turn);
   }
   return turn;
 };
