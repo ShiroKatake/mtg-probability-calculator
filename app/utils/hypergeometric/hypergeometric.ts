f7ed1864a41d03e44bb03b685abfc2b009758dda
ShiroKatake <tgkhang262@gmail.com>
2026-03-28 13:46:03 +1100

    fix(utils): grouped exports and imports into one line

--- app/utils/hypergeometric/hypergeometric.ts
+++ app/utils/hypergeometric/hypergeometric.ts
@@ -1,5 +1,7 @@
-import { combination } from "../combination/combination";
-import { round } from "../round/round";
+import {CardGroup} from "@/app/hooks/useCardGroups";
+import {combination} from "../combination/combination";
+import {round} from "../round/round";
+import {rootCertificates} from "tls";
 
 /**
  * Calculates the probability of x successes in n draws from a population of size N,
@@ -12,16 +14,111 @@ import { round } from "../round/round";
  * @returns The probability of x successes in n draws from a population of size N
  */
 
+// export const hypergeometric = (N: number, n: number, K: number, k: number) => {
+//   return combination(K, k) * combination(N - K, n - k) / combination(N, n);
+// };
+
 export const hypergeometric = (N: number, n: number, K: number, k: number) => {
-  return combination(K, k) * combination(N - K, n - k) / combination(N, n);
+  return (combination(K, k) * combination(N - K, n - k)) / combination(N, n);
 };
 
-export const hypergeometricKOrMore = (N: number, n: number, K: number, k: number) => {
+export const hypergeometricDynamic = (
+  deckSize: number,
+  handSize: number,
+  cardGroups: Pick<CardGroup, "desiredInDeck" | "desiredExpectedInHand">[],
+) => {
+  if (cardGroups.length === 0) return 0;
+
+  const pToDrawDesiredPerGroup = cardGroups.map(({desiredInDeck, desiredExpectedInHand}) =>
+    combination(desiredInDeck, desiredExpectedInHand),
+  );
+  const pToDrawAnyDesired = pToDrawDesiredPerGroup.reduce((acc, cur) => acc * cur);
+
+  const totalDesiredInDeck = cardGroups.map(({desiredInDeck}) => desiredInDeck);
+  const totalDesiredExpectedInHand = cardGroups.map(
+    ({desiredExpectedInHand}) => desiredExpectedInHand,
+  );
+  const pToDrawAnyUndesired = combination(
+    deckSize - totalDesiredInDeck.reduce((acc, cur) => acc + cur, 0),
+    handSize - totalDesiredExpectedInHand.reduce((acc, cur) => acc + cur, 0),
+  );
+
+  const pToDrawAnyCard = combination(deckSize, handSize);
+
+  return (pToDrawAnyDesired * pToDrawAnyUndesired) / pToDrawAnyCard;
+};
+
+type Result = {
+  i: number;
+  j: number;
+  probability: number;
+};
+
+export const hypergeometricComprehensive = (
+  deckSize: number,
+  handSize: number,
+  cardGroups: Pick<CardGroup, "desiredInDeck">[],
+) => {
+  if (cardGroups.length !== 2) {
+    return {
+      table: [],
+      sortedFlatArray: [],
+    };
+  }
+
+  const resultTable: number[][] = [];
+
+  const iMax = Math.min(handSize, cardGroups[0].desiredInDeck);
+  const jMax = Math.min(handSize, cardGroups[1].desiredInDeck);
+
+  for (let i = 0; i <= iMax; i++) {
+    resultTable.push(new Array(jMax + 1).fill(""));
+  }
+
+  const resultFlattened: Result[] = [];
+
+  for (let i = 0; i <= iMax; i++) {
+    for (let j = 0; j <= Math.min(handSize - i, cardGroups[1].desiredInDeck); j++) {
+      const probability =
+        (combination(cardGroups[0].desiredInDeck, i) *
+          combination(cardGroups[1].desiredInDeck, j) *
+          combination(
+            deckSize - cardGroups[0].desiredInDeck - cardGroups[1].desiredInDeck,
+            handSize - i - j,
+          )) /
+        combination(deckSize, handSize);
+      resultTable[i][j] = probability;
+      resultFlattened.push({i, j, probability});
+    }
+  }
+
+  return {
+    table: resultTable,
+    sortedFlatArray: resultFlattened.toSorted((a, b) => b.probability - a.probability),
+  };
+};
+
+export const hypergeometricKOrMore = (
+  N: number,
+  n: number,
+  K: number,
+  k: number,
+  callback?: (probability: number, index: number) => void,
+) => {
   let probability = 0;
   for (let i = 0; i < k; i++) {
     probability += hypergeometric(N, n, K, i);
+    callback?.(probability, i);
   }
   probability = 1 - probability;
+  return probability;
+};
+
+export const hypergeometricKOrLess = (N: number, n: number, K: number, k: number) => {
+  let probability = 0;
+  for (let i = 0; i <= k; i++) {
+    probability += hypergeometric(N, n, K, i);
+  }
   return probability;
 };
 
--- app/utils/probability/successiveEvents.ts
+++
@@ -1,3 +0,0 @@
-export const successiveEvents = (failChance: number, successChance: number, attemptCount: number) => {
-  const probability = Math.pow(failChance, attemptCount - 1) * successChance;
-};
